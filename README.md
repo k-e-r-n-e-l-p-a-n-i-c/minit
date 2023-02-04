3bcbaba8a21a70bd2a9a5eca7800addc54976c5a
arunprasad <arunmudaliar@ymail.com>
2023-02-04 23:26:51 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # minit
+A minimalistic init system written in Golang for educational purpose.
+
+# Motivation
+First of all it's *fun*. You will have to do a fair amount of study about production grade init system's to understand the internal working's of UNIX/Linux init system's. You get to know about how signals and traps work and how to handle zombie and orphan processes.Developing a working model for your own study helps to underdstand many fundamentals that a modern init system comprises of. 
+
+# Features
+*minit*, at the very basic level, does the below:
+* ForkExec a process that you pass to it during it's start up
+* Listens to OS signals and forwards it to the spawned process
+* Does *Zombie* reaping
+
+# Usage
+You can use *minit* inside a container as an init process or build a custom rootFS image with *minit*  placed under /sbin/minit for use with Firecracker.
+
+# References
+(https://github.com/ramr/go-reaper "go-reaper")
+(https://github.com/pablo-ruth/go-init "go-init")
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	go reapZombies()
+	command := os.Args
+
+	if len(command) == 1 {
+		fmt.Println("[init] nothing to run")
+		os.Exit(1)
+	}
+	bin := command[1]
+	args := command[2:]
+	fmt.Printf("[init] %v %v\n", bin, strings.Join(args, " "))
+	cmd := exec.Command(bin, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Create a channel of type os.signal to receive the signals
+	sigs := make(chan os.Signal, 1)
+	defer close(sigs)
+
+	// Use signal.Notify() to trap and relay required signals to our channel
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGABRT, syscall.SIGTERM)
+	defer signal.Reset()
+
+	// Set ProcessGroupID for child process as init process. Both will be under same process group
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	// Start a routine that will receive the signals on the channel and will forward it to child process
+	go func() {
+		sig := <-sigs
+		fmt.Printf("[init] received  %v signal for PID %v\n", sig, cmd.Process.Pid)
+		cmd.Process.Signal(sig)
+	}()
+
+	// Uncomment for testing zombie process creation
+	//n := 1
+	//for n < 4 {
+	//	_, _, _ = syscall.StartProcess("/usr/bin/sleep", []string{"sleep", "40000"}, &syscall.ProcAttr{})
+	//	n++
+	//}
+
+	// Start the command
+	err := cmd.Start()
+	if err != nil {
+		fmt.Printf("[init] failed to start process  %v\n", err)
+		os.Exit(1)
+	}
+
+	// uncomment below delay for testing reaping. This simulates a delay after interrupt signal is trapped
+	// you will find the reaping still happening.
+	//time.Sleep(10 * time.Second)
+
+	// Blocking code using wait()
+	cmd.Wait()
+
+}
+
+func reapZombies() {
+	fmt.Println("[init] starting reaper")
+	for {
+		var wstatus syscall.WaitStatus
+
+		pid, err := syscall.Wait4(-1, &wstatus, syscall.WNOHANG, nil)
+
+		// Below block is required for busy systems
+		for syscall.EINTR == err {
+			pid, err = syscall.Wait4(pid, &wstatus, syscall.WNOHANG, nil)
+		}
+
+		if pid <= 0 {
+			time.Sleep(1 * time.Second)
+		} else {
+			fmt.Printf("[init] reaping zombie %v\n", pid)
+			continue
+		}
+	}
+}
